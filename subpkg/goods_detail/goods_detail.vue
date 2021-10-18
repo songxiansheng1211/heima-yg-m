4fc20fe708f87d679b505cc777758a554386f54e
songhao <979299424@qq.com>
2021-10-18 14:08:30 +0800

    首页 success

---
+++ subpkg/goods_detail/goods_detail.vue
@@ -0,0 +1,18 @@
+<template>
+  <view>
+    详情页
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {   
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
